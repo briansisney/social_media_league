ba306e484b228014068a1da1508036bef3c9e72a
Brian Sisney <briansisney@gmail.com>
2014-02-15 12:18:25 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialMediaLeague::Application.config.secret_key_base = '70a0ee7031e3e6f538c3beda2e6e19884aeb837f6fda862192f4c7a7dccda59ebd857a352077449e35a6cc846a374e36b70deba6dd4a69e7f22c9e419edb7c8e'
